c9860e853ffdfb408955919462032af333bf0a28
Jin Wen Tay <jinwentay@Jins-MacBook-Pro.local>
2019-08-23 20:42:54 +0800

    Level1 Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,18 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________\n"
+                + logo);
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        System.out.println("____________________________________________________________");
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = input.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
